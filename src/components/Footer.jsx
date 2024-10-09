2cb9de72c50c4dcc4e1f2f279646a86a757fcc8c
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2024-10-10 03:24:24 +0530

    Projects Setup v1.0 - Create separate file for Projects Data - Add Tooltip

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,55 +18,53 @@ export default function Footer() {
   }, []);
 
   return (
-    <>
-      <footer>
-        <div className="f-container">
-          <div className="f-left">
-            <h1>Contact Me</h1>
-            <p>
-              Please get in touch if you think our work could be mutually
-              beneficial!
-            </p>
-            <div className="phone">
-              <Icon path={mdiPhone} size={1} className="svg" />
-              <p>+91 9958026543</p>
-            </div>
-            <div className="email">
-              <Icon path={mdiEmail} size={1} className="svg" />
-              <p>hriday2602@gmail.com</p>
-            </div>
-            <div className="socials">
-              <Tooltip title="My Github" placement="bottomRight">
-                <a href="https://github.com/Hriday2406" target="_blank">
-                  <Icon path={mdiGithub} size={1.5} className="svg" />
-                </a>
-              </Tooltip>
-              <Tooltip title="My LinkedIn" placement="bottom">
-                <a
-                  href="https://www.linkedin.com/in/hriday-budhiraja/"
-                  target="_blank"
-                >
-                  <Icon path={mdiLinkedin} size={1.5} className="svg" />
-                </a>
-              </Tooltip>
-              <Tooltip title="My Resume" placement="bottomLeft">
-                <a href="https://bit.ly/hriday2406-resume" target="_blank">
-                  <Icon path={mdiFileAccount} size={1.5} className="svg" />
-                </a>
-              </Tooltip>
-            </div>
+    <footer>
+      <div className="f-container">
+        <div className="f-left">
+          <h1>Contact Me</h1>
+          <p>
+            Please get in touch if you think our work could be mutually
+            beneficial!
+          </p>
+          <div className="phone">
+            <Icon path={mdiPhone} size={1} className="svg" />
+            <p>+91 9958026543</p>
           </div>
-          <div className="f-right">
-            <img
-              className="img2"
-              id="img2"
-              src="./dp2.jpg"
-              alt="My Image"
-              ref={dp2Ref}
-            />
+          <div className="email">
+            <Icon path={mdiEmail} size={1} className="svg" />
+            <p>hriday2602@gmail.com</p>
+          </div>
+          <div className="socials">
+            <Tooltip title="My Github" placement="bottomRight">
+              <a href="https://github.com/Hriday2406" target="_blank">
+                <Icon path={mdiGithub} size={1.5} className="svg" />
+              </a>
+            </Tooltip>
+            <Tooltip title="My LinkedIn" placement="bottom">
+              <a
+                href="https://www.linkedin.com/in/hriday-budhiraja/"
+                target="_blank"
+              >
+                <Icon path={mdiLinkedin} size={1.5} className="svg" />
+              </a>
+            </Tooltip>
+            <Tooltip title="My Resume" placement="bottomLeft">
+              <a href="https://bit.ly/hriday2406-resume" target="_blank">
+                <Icon path={mdiFileAccount} size={1.5} className="svg" />
+              </a>
+            </Tooltip>
           </div>
         </div>
-      </footer>
-    </>
+        <div className="f-right">
+          <img
+            className="img2"
+            id="img2"
+            src="./dp2.jpg"
+            alt="My Image"
+            ref={dp2Ref}
+          />
+        </div>
+      </div>
+    </footer>
   );
 }
