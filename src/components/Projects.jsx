d175b595859caf31beb478ab7011c318069507b4
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2024-10-10 22:24:06 +0530

    Projects Setup v2.0 - Add tags for tech stack

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,56 @@
 import { useEffect, useRef } from "react";
 import Icon from "@mdi/react";
-import { mdiGithub, mdiOpenInNew } from "@mdi/js";
-import { Tooltip } from "antd";
+import {
+  mdiGithub,
+  mdiOpenInNew,
+  mdiLanguageHtml5,
+  mdiLanguageCss3,
+  mdiLanguageJavascript,
+  mdiReact,
+  mdiTailwind,
+  mdiGit,
+} from "@mdi/js";
+import { Tooltip, Tag } from "antd";
 import { observer } from "../utils/utils";
 import { PROJECTDATA } from "../utils/constants";
+
+function TechTags({ tag }) {
+  let tagIcon;
+
+  switch (tag) {
+    case "HTML5": {
+      tagIcon = <Icon path={mdiLanguageHtml5} size={1} color="#e34c26" />;
+      break;
+    }
+    case "CSS3": {
+      tagIcon = <Icon path={mdiLanguageCss3} size={1} color="#264de4" />;
+      break;
+    }
+    case "Js": {
+      tagIcon = <Icon path={mdiLanguageJavascript} size={1} color="#f0db4f" />;
+      break;
+    }
+    case "React": {
+      tagIcon = <Icon path={mdiReact} size={1} color="#61dafb" />;
+      break;
+    }
+    case "Tailwind": {
+      tagIcon = <Icon path={mdiTailwind} size={1} color="#06B6D4" />;
+      break;
+    }
+    case "Git": {
+      tagIcon = <Icon path={mdiGit} size={1} color="#f34f29" />;
+      break;
+    }
+    default: {
+    }
+  }
+  return (
+    <Tooltip title={tag} placement="bottom">
+      <Tag color="#00000000">{tagIcon}</Tag>
+    </Tooltip>
+  );
+}
 
 export default function Projects() {
   const cardsRef = useRef([]);
@@ -45,7 +92,14 @@ export default function Projects() {
                     </a>
                   </div>
                 </div>
-                <p>{project.desc}</p>
+                <div className="card-bottom">
+                  <p>{project.desc}</p>
+                  <div className="tech">
+                    {project.tech.map((tag, i) => {
+                      return <TechTags tag={tag} key={project.title + i} />;
+                    })}
+                  </div>
+                </div>
               </div>
             </div>
           );
